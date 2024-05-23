d3f4c7249aaade5ca66cd27296c0299ccc6958a6
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2024-05-22 22:16:01 -0300

    removendo yt

--- data/socials.js
+++ data/socials.js
@@ -4,21 +4,17 @@ let socials = [
         img: require('@/assets/socials/linkedin.png'),
         link: 'https://www.linkedin.com/in/arthurmirandadev/'
     },
-    { 
-        title: 'GitHub',
-        img: require('@/assets/socials/github.png'),
-        link: 'https://github.com/devarthurmiranda'
-    },
     {
         title: 'Instagram',
         img: require('@/assets/socials/instagram.png'),
         link: 'https://www.instagram.com/arthurmirandadev/'
     },
     { 
-        title: 'Youtube',
-        img: require('@/assets/socials/youtube.png'),
-        link: 'https://www.youtube.com/channel/UCMUWFKLaq4LrxjzxhjYqAsA'
+        title: 'GitHub',
+        img: require('@/assets/socials/github.png'),
+        link: 'https://github.com/devarthurmiranda'
     },
+
 ];
 
 export default socials;
