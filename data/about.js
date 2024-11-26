92a7828fa3ce08bcf70e7f98d9ff7228e125fb9a
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2024-11-25 21:31:17 -0300

    Update: updating data

--- data/about.js
+++ data/about.js
@@ -1,7 +1,7 @@
 let about = {
         title: 'Sobre mim',
         first_paragraph: 'Comecei a programar em 2020, pouco antes de entrar na faculdade, onde me apaixonei pelo desenvolvimento de Software. Atualmente estou participando de projetos na empresa júnior PekiCode, onde tenho a oportunidade de trabalhar com tecnologias como Flutter e NodeJS.',
-        second_paragraph: 'Como pessoa, tenho 20 anos e gosto de sempre estar aprendendo algo novo.',
+        second_paragraph: 'Como pessoa, gosto de sempre estar aprendendo algo novo e participando de eventos.',
         third_paragraph: 'Meu objetivo é garantir que os projetos que participo sejam entregues com a melhor qualidade possível, e que possam ajudar as pessoas de alguma forma.',
 }
 
