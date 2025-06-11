dcd5620b032f6cdbc07b17550d4d1b1692f1c24f
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2025-06-11 17:59:16 -0300

    UPDATE: About

--- data/about.js
+++ data/about.js
@@ -1,7 +1,7 @@
 let about = {
         title: 'Sobre mim',
-        first_paragraph: 'Desde que comecei a desenvolver software sempre tive o objetivo de ajudar pessoas, e hoje, depois de anos de estudo e trabalho, posso dizer que desenvolvi projetos como um modelo de IA para combater crimes nas redes sociais e fui diretor de projetos da empresa júnior Pekicode, que foi inovadora na sua proposta de inserir acadêmicos de ensino superior no mercado de trabalho da região.',
-        second_paragraph: 'Passei pelo mercado de trabalho com a Techify, empresa focada em treinamentos de automação e inteligência artificial, e também desenvolvi soluções diversas para operações internas em outras empresas.',
+        first_paragraph: 'Desde que comecei a desenvolver software sempre tive o objetivo de ajudar pessoas, e hoje, depois de anos de estudo e trabalho, posso dizer que desenvolvi projetos como um modelo de IA para combater crimes nas redes sociais e trabalhei direta ou indiretamente com empresas como a Techify, Redes Brasil, TopNet, PekiCode, Complem entre outras.',
+        second_paragraph: 'Atualmente trabalho na Techify, empresa focada em treinamentos de automação e inteligência artificial, fui diretor de projetos da empresa júnior Pekicode, que foi inovadora em sua proposta de introduzir acadêmicos de ensino superior ao mercado de trabalho regional. Além disso, também desenvolvi soluções diversas para operações internas em outras empresas.',
         third_paragraph: 'Sou apaixonado na área de computação e ciência em geral, sempre buscando aperfeiçoamento e novos desafios.',
 }
 
