8c34ad0981199dc2570c7ebc245abb8f0b8854b5
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2025-08-04 08:33:51 -0300

    Update: Updating new skills

--- data/skills.js
+++ data/skills.js
@@ -1,8 +1,10 @@
 let skills = [
-    { name: 'Vue', icon: require('@/assets/skills/vue.png'), wiki: 'https://en.wikipedia.org/wiki/Vue.js' },
+    { name: 'Python', icon: require('@/assets/skills/python.png'), wiki: 'https://en.wikipedia.org/wiki/Python_(programming_language)' },
+    { name: 'N8N', icon: require('@/assets/skills/n8n.png'), wiki: 'https://n8n.io/' },
+    { name: 'Docker', icon: require('@/assets/skills/docker.png'), wiki: 'https://en.wikipedia.org/wiki/Docker_(software)' },
     { name: 'Node', icon: require('@/assets/skills/node.png'), wiki: 'https://en.wikipedia.org/wiki/Node.js' },
     { name: 'Flutter', icon: require('@/assets/skills/flutter.png'), wiki: 'https://en.wikipedia.org/wiki/Flutter_(software)' },
-    { name: 'Java', icon: require('@/assets/skills/java.png'), wiki: 'https://en.wikipedia.org/wiki/Java_(programming_language)' },
+    { name: 'Java', icon: require('@/assets/skills/java.png'), wiki: 'https://en.wikipedia.org/wiki/Java_(programming_language)' }
 
 ]
 
