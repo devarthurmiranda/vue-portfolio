1559e22b2fb97a6202292bc1577a206fbee33c56
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2025-12-17 19:45:16 -0300

    refactor(data): update organization descriptions in orgs.js

--- data/orgs.js
+++ data/orgs.js
@@ -1,7 +1,7 @@
 let orgs =[
-    { name: 'Techify', link:'https://www.linkedin.com/company/techifybrasil/', occupation: 'Contributed as a Full‑Stack Developer, building automation and AI solutions for partner companies such as Redes Brasil. I strengthened technical skills in programming, automation, and AI.'},
-    { name: 'PekiCode', link:'https://www.linkedin.com/company/peki-code/', occupation: 'Contribute as a Project Manager and Full‑Stack Developer, leading the development team and ensuring the delivery of high‑quality projects. I developed leadership, teamwork, and people management skills.'},
-    { name: 'TopNet', link:'https://www.facebook.com/topnetprovedordeinternet/?locale=pt_BR', occupation: 'Contributed as Technical Support, handling customer tickets and configuring routers. I gained skills in computer networking, troubleshooting, and communication.'},
+    { name: 'Techify', link:'https://www.linkedin.com/company/techifybrasil/', occupation: 'Data Scientist - building AI and Automation solutions  on a national scale.'},
+    { name: 'PekiCode', link:'https://www.linkedin.com/company/peki-code/', occupation: 'Project Director - leading the development team and ensuring the delivery of high‑quality projects, bridging higher-education talent to the regional job market.'},
+    { name: 'TopNet', link:'https://www.facebook.com/topnetprovedordeinternet/?locale=pt_BR', occupation: 'Technical Support - handling customer tickets and configuring routers.'},
   ];
 
 export default orgs;
