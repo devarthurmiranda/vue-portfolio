45f3f21c1b1b64b35aeba77800ad57d7c3f04f4e
Arthur Miranda <contato.arthurmorais.dev@gmail.com>
2026-02-11 07:34:21 -0300

    feat: add Cloudflare and Coolify skills with their respective icons and data

--- data/skills.js
+++ data/skills.js
@@ -3,11 +3,9 @@ let skills = [
     { name: 'N8N', icon: require('@/assets/skills/n8n.png'), wiki: 'https://n8n.io/' },
     { name: 'Docker', icon: require('@/assets/skills/docker.png'), wiki: 'https://en.wikipedia.org/wiki/Docker_(software)' },
     { name: 'Node', icon: require('@/assets/skills/node.png'), wiki: 'https://en.wikipedia.org/wiki/Node.js' },
-    { name: 'Flutter', icon: require('@/assets/skills/flutter.png'), wiki: 'https://en.wikipedia.org/wiki/Flutter_(software)' },
-    { name: 'Java', icon: require('@/assets/skills/java.png'), wiki: 'https://en.wikipedia.org/wiki/Java_(programming_language)' },
-    { name: 'Vue', icon: require('@/assets/skills/vue.png'), wiki: 'https://en.wikipedia.org/wiki/Vue.js' },
-    { name: 'PostgreSQL', icon: require('@/assets/skills/postgresql.png'), wiki: 'https://en.wikipedia.org/wiki/PostgreSQL' }
-
+    { name: 'PostgreSQL', icon: require('@/assets/skills/postgresql.png'), wiki: 'https://en.wikipedia.org/wiki/PostgreSQL' },
+    { name: 'Cloudflare', icon: require('@/assets/skills/cloudflare.png'), wiki: 'https://en.wikipedia.org/wiki/Cloudflare' },
+    { name: 'Coolify', icon: require('@/assets/skills/coolify.png'), wiki: 'https://coolify.io/' }
 ]
 
 export default skills;
