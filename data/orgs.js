5ca8804b07ca4a6552cdad0e1a9fe8beb20875f5
Arthur Miranda <contact.arthur.miranda.dev@gmail.com>
2026-04-02 11:48:02 -0300

    UPDATE: Changing job title

--- data/orgs.js
+++ data/orgs.js
@@ -1,5 +1,5 @@
 let orgs =[
-    { name: 'Techify', link:'https://www.linkedin.com/company/techifybrasil/', occupation: 'Data Scientist - building AI and Automation solutions  on a national scale.'},
+    { name: 'Techify', link:'https://www.linkedin.com/company/techifybrasil/', occupation: 'AI Engineer - building AI and Automation solutions  on a national scale.'},
     { name: 'PekiCode', link:'https://www.linkedin.com/company/peki-code/', occupation: 'Project Director - leading the development team and ensuring the delivery of high‑quality projects, bridging higher-education talent to the regional job market.'},
     { name: 'TopNet', link:'https://www.facebook.com/topnetprovedordeinternet/?locale=pt_BR', occupation: 'Technical Support - handling customer tickets and configuring routers.'},
   ];
