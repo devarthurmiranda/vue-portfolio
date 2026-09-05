9adf20fe695a4743dca25fcdf6e7a06a65cee455
Arthur Miranda <contact.arthur.miranda.dev@gmail.com>
2026-09-04 23:32:45 -0300

    update

--- data/intro.js
+++ data/intro.js
@@ -1,6 +1,6 @@
 let intro = {
     title: "Hi traveler, my name is Arthur Miranda!",
-    description: "Data Engineer at Delfos | Building Enterprise AI Agents & Systems | Scaling Operations through Intelligence."
+    description: "Data Engineer at Delfos | Scaling Operations through Intelligence."
 };
 
 export default intro;
